267aacdb625adf965f6d77562a4c395dfeb3eddf
gaurav kumar <gauravkumarskg851215@gmail.com>
2026-02-12 02:46:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
- let prices = document.querySelectorAll(".prices");
+  let prices = document.querySelectorAll(".prices");
 let total = 0;
 
 prices.forEach((price) => {
     total += Number(price.textContent);
 });
 
-let table = document.getElementById("groceryTable");
+let table = document.getElementById("groceryTable"); 
 
 let newRow = document.createElement("tr");
 let newCell = document.createElement("td");
