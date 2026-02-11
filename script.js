2a781e20fddd883a2afe776078054ef6e93b1266
gaurav kumar <gauravkumarskg851215@gmail.com>
2026-02-12 02:50:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ prices.forEach((price) => {
     total += Number(price.textContent);
 });
 
-let table = document.getElementById("groceryTable"); 
+let table = document.getElementById("groceryTable");
+let tbody = table.querySelector("tbody") || table;
 
 let newRow = document.createElement("tr");
 let newCell = document.createElement("td");
@@ -14,4 +15,4 @@ newCell.textContent = total;
 newCell.colSpan = 2;
 
 newRow.appendChild(newCell);
-table.appendChild(newRow);
+tbody.appendChild(newRow);
